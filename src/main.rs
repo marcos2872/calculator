93fdbfafd44f8195d7a7bdede71f337250e9c471
Marcos Brito <marcos.brito@sp.senai.br>
2025-11-11 14:27:07 -0300

    Refactor calculator UI: update event handlers for clean, delete, and add character functions; improve calculation logic and UI layout

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
+use slint::SharedString;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_weak = ui.as_weak();
-    
-    ui.on_increment_counter(move || {
+    ui.on_clean(move || {
         let ui = ui_weak.unwrap();
-        let current = ui.get_counter();
-        ui.set_counter(current + 1);
+        ui.set_calc(SharedString::from("0"));
     });
 
     let ui_weak = ui.as_weak();
-    ui.on_decrement_counter(move || {
+    ui.on_delete(move || {
         let ui = ui_weak.unwrap();
-        let current = ui.get_counter();
-        ui.set_counter(current - 1);
+        let current = ui.get_calc();
+        let new_value = if current.len() <= 1 {
+            "0"
+        } else {
+            current.get(..current.len() - 1).unwrap_or("0")
+        };
+        ui.set_calc(SharedString::from(new_value));
+    });
+
+    let ui_weak = ui.as_weak();
+    ui.on_add_character(move |value| {
+        let ui = ui_weak.unwrap();
+        let current = ui.get_calc();
+        
+        let new_calc = if current == "0" && !value.starts_with('.') && !is_operator(&value) {
+            value.to_string()
+        } else if is_operator(&current[&current.len() - 1..]) && is_operator(&value) {
+            current.to_string()
+        } else {
+            format!("{}{}", current, value)
+        };
+        
+        ui.set_calc(SharedString::from(new_calc));
+    });
+
+    let ui_weak = ui.as_weak();
+    ui.on_calculate(move || {
+        let ui = ui_weak.unwrap();
+        let expression = ui.get_calc();
+
+        let calc = 
+        
+        ui.set_calc(SharedString::from("0"));
     });
 
     ui.run()
 }
+
+fn is_operator(s: &str) -> bool {
+    matches!(s, "+" | "-" | "x" | "÷" | "%")
+}   
